eceb9781f9afbdbb87d4bbe706bad98ce4855655
Matt Deeds <mdeeds@gmail.com>
2021-09-12 10:11:13 -0700

    Keyboard works - needs quantization

--- keyboardState.js
+++ keyboardState.js
@@ -1,26 +1,34 @@
 class KeyboardState {
   constructor() {
+    console.log('initializing keyboard');
     this.keysDown = new Set();
     this.freshKeys = new Set();
     const body = document.getElementsByTagName('body')[0];
-    body.addEventListener('keydown', (ev) => { this.handle(ev);});
-    body.addEventListener('keyup', (ev) => { this.handle(ev);});
+    body.addEventListener('keydown', (ev) => { this.handle(ev); });
+    body.addEventListener('keyup', (ev) => { this.handle(ev); });
   }
-  
+
   handle(ev) {
+    console.log(`${ev.type}: ${ev.code}`);
     switch (ev.type) {
-      case 'down': this.keysDown.add(ev.code); this.freshKeys.add(ev.code); break;
-      case 'up': this.keysDown.delete(ev.code); break;
+      case 'keydown':
+        if (!this.keysDown.has(ev.code)) {
+          this.keysDown.add(ev.code);
+          this.freshKeys.add(ev.code);
+        }
+        break;
+      case 'keyup': this.keysDown.delete(ev.code); break;
     }
   }
-  
+
   isDown(code) {
     return this.keysDown.has(code);
   }
-  
+
   justPressed(code) {
     if (this.freshKeys.has(code)) {
       this.freshKeys.delete(code);
+      console.log(`Got: ${code}`);
       return true;
     } else {
       return false;
