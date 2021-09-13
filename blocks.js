1694894279e06afaf0b2a9071f26e2b7a152c3a5
Glitch (motley-booming-hunter) <none>
2021-09-13 02:26:18 +0000

    🛣🎐 Updated with Glitch

--- blocks.js
+++ blocks.js
@@ -1,7 +1,11 @@
+function indexToTheta(index) {
+  return (index * 2 * Math.PI) / 16 - Math.PI;
+}
+
 class FlyingBlock {
   constructor(scene, n) {
     this.box = document.createElement("a-box");
-    this.toTheta = (n * 2 * Math.PI) / 16 - Math.PI;
+    this.toTheta = indexToTheta(n);
     this.p = 0;
     this.box.object3D.rotation.set(0, -this.toTheta, 0);
     scene.appendChild(this.box);
@@ -25,51 +29,65 @@ class FlyingBlock {
   }
 }
 
+class PlayableBlock {
+  constructor(keyboardState, keyCode, url, sceneEl, trackIndex) {
+    this.keyboardState = keyboardState;
+    this.code = keyCode;
+    const theta = indexToTheta(trackIndex);
+    this.audio = new Audio(url);
+    this.box = document.createElement("a-box");
+    this.box.object3D.position.set(Math.cos(theta) * 4, 0.5, Math.sin(theta) * 4);
+    this.box.setAttribute("color", "#55f");
+    this.box.setAttribute("opacity", 0.5);
+    this.box.object3D.rotation.set(0, -theta, 0);
+    sceneEl.appendChild(this.box);
+    this.box.addEventListener("mouseenter", () => {
+      this.box.setAttribute("color", "#f55");
+      this.audio.currentTime = 0;
+      this.audio.play();
+    });
+    this.box.addEventListener("mouseleave", () => {
+      this.box.setAttribute("color", "#5f5");
+    });
+    this.box.addEventListener("mousedown", () => {
+    });
+    this.box.classList.add("clickable");
+  }
+
+  tick(timeMs, timeDeltaMs) {
+    const prevAngle = this.box.object3D.rotation.toArray()[1];
+    const bpm = 96;
+    const quarterTurnDuration = 1000 * 60 / bpm;
+    this.box.object3D.rotation.set(
+      0, prevAngle + Math.PI / 2 * timeDeltaMs / quarterTurnDuration, 0);
+    if (this.keyboardState.justPressed(this.code)) {
+      this.audio.currentTime = 0;
+      this.audio.play();
+    }
+  }
+}
+
 class PlayableBlocks {
   constructor(keyboardState) {
     const sceneEl = document.querySelector("a-scene");
-    this.clips = [];
-    this.clips.push(
-      new Audio(
-        "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Fkick.wav?v=1631392733145"
-      )
-    );
-    this.clips.push(
-      new Audio(
-        "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Fhats.wav?v=1631392739980"
-      )
-    );
-    this.clips.push(
-      new Audio(
-        "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Fvirtual.wav?v=1631392748787"
-      )
-    );
-    this.clips.push(
-      new Audio(
-        "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Freality.wav?v=1631392757731"
-      )
-    );
-    let theta = -Math.PI + 3 * Math.PI / 8;
-    
-    for (const clip of this.clips) {
-      const box = document.createElement("a-box");
-      box.object3D.position.set(Math.cos(theta) * 4, 0.5, Math.sin(theta) * 4);
-      box.setAttribute("color", "#55f");
-      box.setAttribute("opacity", 0.5);
-      box.object3D.rotation.set(0, -theta, 0);
-      sceneEl.appendChild(box);
-      box.addEventListener("mouseenter", () => {
-        box.setAttribute("color", "#f55");
-        clip.currentTime = 0;
-        clip.play();
-      });
-      box.addEventListener("mouseleave", () => {
-        box.setAttribute("color", "#5f5");
-      });
-      box.addEventListener("mousedown", () => {
-      });
-      box.classList.add("clickable");
-      theta += (2 * Math.PI) / 16;
+    this.pbs = [];
+    this.pbs.push(new PlayableBlock(keyboardState, 'Digit1',
+      "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Fkick.wav?v=1631392733145",
+      sceneEl, 3));
+    this.pbs.push(new PlayableBlock(keyboardState, 'Digit2',
+      "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Fhats.wav?v=1631392739980",
+      sceneEl, 4));
+    this.pbs.push(new PlayableBlock(keyboardState, 'Digit3',
+      "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Fvirtual.wav?v=1631392748787",
+      sceneEl, 5));
+    this.pbs.push(new PlayableBlock(keyboardState, 'Digit4',
+      "https://cdn.glitch.com/19df276e-5dfe-4bab-915a-410c481a8b0d%2Freality.wav?v=1631392757731",
+      sceneEl, 6));
+  }
+
+  tick(timeMs, timeDeltaMs) {
+    for (const pb of this.pbs) {
+      pb.tick(timeMs, timeDeltaMs);
     }
   }
 }
@@ -83,13 +101,13 @@ class FlyingBlocks {
     this.flyingBlocks = [];
   }
 
-  tick(time, timeDelta) {
+  tick(timeMs, timeDeltaMs) {
     const camera = document.querySelector("a-camera");
     camera.object3D.getWorldQuaternion(this.cameraQuaternion);
     this.cameraQuaternion.normalize();
     const a = this.cameraQuaternion.toArray();
     const cameraAngle = -2 * Math.atan2(a[1], a[3]) - 0.5 * Math.PI;
-    for (let i = 0; i < this.flyingBlocks.length; ) {
+    for (let i = 0; i < this.flyingBlocks.length;) {
       const fb = this.flyingBlocks[i];
       fb.render(cameraAngle);
       if (fb.box) {
@@ -100,7 +118,7 @@ class FlyingBlocks {
     }
     ++this.frameNumber;
   }
-  
+
   getFactory() {
     return (trackIndex) => {
       this.flyingBlocks.push(new FlyingBlock(this.sceneEl, trackIndex));
