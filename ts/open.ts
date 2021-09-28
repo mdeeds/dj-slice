1cc1c210f9e37dad1c86a58c79342a3de2134de3
Matt Deeds <mdeeds@gmail.com>
2021-09-28 16:00:59 -0700

    Allow respositioning of parts.

--- ts/open.ts
+++ ts/open.ts
@@ -48,6 +48,18 @@ function renderBuffer(
   for (let i = 0; i < frames.length; ++i) {
     const y = 200 * frames[i];
     ctx.fillRect(x, 300 - y, 1, 2 * y);
+    framesUntilNextBeat -= 1;
+    x += 1000 / frames.length;
+  }
+}
+
+function renderBars(canvas: HTMLCanvasElement, frames: Float32Array, audioCtx: AudioContext, bpm: number) {
+  const ctx = canvas.getContext('2d');
+  const framesPerBeat = Math.round(audioCtx.sampleRate * 60 / bpm);
+  let x = 0;
+  ctx.fillStyle = '#f992';
+  let framesUntilNextBeat = 0;
+  for (let i = 0; i < frames.length; ++i) {
     if (framesUntilNextBeat <= 0) {
       framesUntilNextBeat += framesPerBeat;
       ctx.fillStyle = 'black';
@@ -65,7 +77,15 @@ function initCanvas(body: HTMLElement): HTMLCanvasElement[] {
   peaksCanvas.id = 'peaks';
   peaksCanvas.width = 1000;
   peaksCanvas.height = 600;
+  peaksCanvas.style.setProperty('visibility', 'hidden');
   body.appendChild(peaksCanvas);
+
+  const barsCanvas: HTMLCanvasElement =
+    document.createElement('canvas') as unknown as HTMLCanvasElement;
+  barsCanvas.id = 'bars';
+  barsCanvas.width = 1000;
+  barsCanvas.height = 600;
+  body.appendChild(barsCanvas);
 
   const selectionCanvas: HTMLCanvasElement =
     document.createElement('canvas') as unknown as HTMLCanvasElement;
@@ -74,12 +94,13 @@ function initCanvas(body: HTMLElement): HTMLCanvasElement[] {
   selectionCanvas.height = 600;
   body.appendChild(selectionCanvas);
 
-  return [peaksCanvas, selectionCanvas];
+  return [peaksCanvas, barsCanvas, selectionCanvas];
 }
 
 class Granules {
   private audioBuffer: AudioBuffer;
   private controlPoints: number[];
+  private playbackPoints: number[];
   private activePoint: number;
   private audioCtx: AudioContext;
   private numSamples: number;
@@ -88,6 +109,7 @@ class Granules {
     this.numSamples = audioBuffer.getChannelData(0).length;
     this.audioCtx = audioCtx;
     this.controlPoints = [0, this.numSamples / audioCtx.sampleRate];
+    this.playbackPoints = [0, this.numSamples / audioCtx.sampleRate];
     this.activePoint = 0;
   }
 
@@ -96,8 +118,13 @@ class Granules {
       (this.numSamples / this.audioCtx.sampleRate);
     return x;
   }
+  private getXForPlayback(index: number, canvas: HTMLCanvasElement) {
+    const x = (canvas.width * this.playbackPoints[index]) /
+      (this.numSamples / this.audioCtx.sampleRate);
+    return x;
+  }
 
-  render(canvas: HTMLCanvasElement) {
+  render(canvas: HTMLCanvasElement, peaksCanvas: HTMLCanvasElement) {
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = '#66f'
     for (let i = 0; i < this.controlPoints.length - 1; ++i) {
@@ -111,17 +138,28 @@ class Granules {
         ctx.fillStyle = '#ddd'
       }
       ctx.fillRect(
-        this.getXForPoint(i, canvas), 0,
-        width, canvas.height); 1
+        this.getXForPlayback(i, canvas), 0,
+        width, canvas.height);
+
+      ctx.drawImage(peaksCanvas,
+        this.getXForPoint(i, canvas), 0, width, canvas.height,
+        this.getXForPlayback(i, canvas), 0, width, canvas.height)
     }
   }
 
   changeStart(delta: number) {
     this.controlPoints[this.activePoint] =
       Math.max(0, this.controlPoints[this.activePoint] + delta);
+    this.playbackPoints[this.activePoint] =
+      Math.max(0, this.playbackPoints[this.activePoint] + delta);
+  }
+  changePlayStart(delta: number) {
+    this.playbackPoints[this.activePoint] =
+      Math.max(0, this.playbackPoints[this.activePoint] + delta);
   }
   changeEnd(delta: number) {
     this.controlPoints[this.activePoint + 1] += delta;
+    this.playbackPoints[this.activePoint + 1] += delta;
   }
   next(dir: number) {
     this.activePoint += dir;
@@ -138,11 +176,32 @@ class Granules {
     audioNode.start(0, this.controlPoints[this.activePoint], duration);
   }
 
-  split() {
-    const mid = (this.controlPoints[this.activePoint] +
-      this.controlPoints[this.activePoint + 1]) / 2;
-    this.controlPoints.splice(this.activePoint + 1, 0, mid);
+  private splitInternal(a: number[], i: number): number {
+    const mid = (a[i] + a[i + 1]) / 2;
+    a.splice(i + 1, 0, mid);
+    return mid;
   }
+
+  split() {
+    this.splitInternal(this.controlPoints, this.activePoint);
+    this.splitInternal(this.playbackPoints, this.activePoint);
+  }
+
+  join() {
+    if (this.controlPoints.length > 2 && this.activePoint > 0) {
+      this.controlPoints.splice(this.activePoint, 1);
+      this.playbackPoints.splice(this.activePoint, 1);
+    }
+  }
+}
+
+function getBpmFromFrames(numFrames: number, audioCtx: AudioContext) {
+  const durationS = numFrames / audioCtx.sampleRate;
+  let bpm = 60 / durationS;
+  while (bpm < 120 * Math.sqrt(0.5)) {
+    bpm *= 2;
+  }
+  return bpm;
 }
 
 async function go() {
@@ -153,23 +212,27 @@ async function go() {
   const buffer = await sample.getData();
   const body = document.getElementsByTagName('body')[0];
 
-  const [peaksCanvas, selectionCanvas] = initCanvas(body);
-  peaksCanvas.tabIndex = 0;
+  const [peaksCanvas, barsCanvas, selectionCanvas] = initCanvas(body);
+  barsCanvas.tabIndex = 0;
   const peaksCtx = peaksCanvas.getContext('2d');
 
   const frames = buffer.getChannelData(0);
-  const bpm = 110;
+  const bpm = getBpmFromFrames(frames.length, audioCtx);
   const granules = new Granules(buffer, audioCtx);
 
   peaksCtx.clearRect(0, 0, peaksCanvas.width, peaksCanvas.height);
   renderBuffer(peaksCtx, peaksCanvas, audioCtx, frames, bpm);
+  renderBars(barsCanvas, frames, audioCtx, bpm);
 
-  granules.render(selectionCanvas);
+  granules.render(selectionCanvas, peaksCanvas);
 
-  peaksCanvas.addEventListener('keydown', (ev: KeyboardEvent) => {
+  barsCanvas.addEventListener('keydown', (ev: KeyboardEvent) => {
     let actionTaken = true;
     switch (ev.code) {
       case 'KeyS': granules.split(); break;
+      case 'KeyJ': granules.changePlayStart(-0.01); break;
+      case 'KeyK': granules.changePlayStart(0.01); break;
+      case 'Delete': case 'Backspace': granules.join(); break;
       case 'ArrowLeft': granules.changeStart(-0.01); break;
       case 'ArrowRight': granules.changeStart(0.01); break;
       case 'ArrowDown': granules.changeEnd(-0.01); break;
@@ -184,10 +247,11 @@ async function go() {
       case 'Space': granules.play(); break;
     }
     if (actionTaken) {
+      console.log(`Action: ${ev.code}`);
       ev.preventDefault();
       selectionCanvas.getContext('2d')
         .clearRect(0, 0, selectionCanvas.width, selectionCanvas.height);
-      granules.render(selectionCanvas);
+      granules.render(selectionCanvas, peaksCanvas);
     }
   });
 }
