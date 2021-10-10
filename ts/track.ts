66c53ffcbaf3a47583af34999903c8a674736a4d
Matt Deeds <mdeeds@gmail.com>
2021-10-09 22:00:20 -0700

    Load JSON Manifest

---
+++ ts/track.ts
@@ -0,0 +1,22 @@
+import * as AFRAME from "aframe";
+import { GameTime } from "./gameTime";
+import { Sample } from "./sample";
+
+export class Track {
+  private samples: Sample[] = [];
+  private images: HTMLImageElement[] = [];
+  private static idNumber = 0;
+  constructor(obj: object, gameTime: GameTime, assets: AFRAME.Entity) {
+    for (const sample of obj['samples']) {
+      const audioUrl = sample['audio'];
+      console.log(`Loading: ${audioUrl}`);
+      const s = new Sample(audioUrl, gameTime);
+      this.samples.push(s);
+      const i = document.createElement('img') as any as HTMLImageElement;
+      i.setAttribute('src', sample['image']);
+      i.id = `trackImage${Track.idNumber++}`
+      this.images.push(i);
+      assets.appendChild(i);
+    }
+  }
+}
